965f21cee9bf67b0eb99126d5e5170072b80f363
Hanette Le <hanette2000@yahoo.com>
2019-11-20 15:35:01 -0800

    Adding player and meat

---
+++ 121-Pitch-1/Assets/Scripts/Pickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private bool held;
+    private void Update()
+    {
+        if (held == true)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }      
+    }
+    public Transform look;
+
+    private void OnMouseDown()
+    {
+        held = true;
+        //GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        this.transform.position = look.position;
+        this.transform.parent = GameObject.Find("Look").transform;
+    }
+
+    private void OnMouseUp()
+    {
+        held = false;
+        this.transform.parent = null;
+        //GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}

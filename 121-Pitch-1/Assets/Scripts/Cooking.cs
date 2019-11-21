6e009db62f5699c50d28aafb83a691852dbb7f31
Hanette Le <hanette2000@yahoo.com>
2019-11-20 17:45:34 -0800

    Added cooking patty

---
+++ 121-Pitch-1/Assets/Scripts/Cooking.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooking : MonoBehaviour {
+    public Material Cooked;
+    public GameObject Meat;
+    public bool isCooked;
+    bool TimerOn;
+    float seconds;
+    float timeLeft = 10.0f; // 10 seconds
+    float curTime;
+
+    void Start(){
+         TimerOn = false;
+         isCooked = false;
+         curTime = 0.0f;
+     }
+
+     void Update(){
+             if (TimerOn){
+                 curTime += .01f;
+                 seconds = (int)(curTime % 60f);
+             }
+             if(curTime >= timeLeft){
+                 Meat.GetComponent<MeshRenderer>().material = Cooked;
+                 isCooked = true;
+             }
+     }
+
+     public void OnCollisionExit(){
+         TimerOn = false;
+     }
+
+    void OnCollisionEnter(Collision other){
+        if (other.gameObject.name == "Meat"){
+            TimerOn = true;
+        }
+    }
+}
